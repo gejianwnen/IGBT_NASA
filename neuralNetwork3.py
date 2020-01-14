34a144ba6843f074390f6acaa7a5675dc65f5e23
gejianwen <1327303553@qq.com>
2020-01-14 08:43:33 +0800

    subsample as 8 peice

--- neuralNetwork3.py
+++ neuralNetwork3.py
@@ -9,19 +9,23 @@ import torch
 from torch import nn
 
 class neuralNetwork2(nn.Module):
-    def __init__(self, in_dim, n_hidden_1, out_dim,droprate = 0.5):
+    def __init__(self, in_dim, n_hidden_1,n_hidden_2, out_dim,droprate = 0.5):
         super(neuralNetwork2, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Linear(in_dim, n_hidden_1),
             nn.Dropout(p = droprate),
             nn.ReLU(True))
         self.layer2 = nn.Sequential(
-            nn.Linear(n_hidden_1, out_dim),
+            nn.Linear(n_hidden_1, n_hidden_2),
+            nn.Dropout(p = droprate),
+            nn.ReLU(True))
+        self.layer3 = nn.Sequential(
+            nn.Linear(n_hidden_2, out_dim),
             nn.ReLU(True))
 
 
     def forward(self, x):
         x = self.layer1(x)
-        
         x = self.layer2(x)
+        x = self.layer3(x)
         return x
